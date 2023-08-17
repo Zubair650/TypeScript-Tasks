e95ed5379e261e251097765787839984f2aada3b
Zubair Ahmed <saad03261@gmail.com>
2023-08-17 19:28:55 +0600

    ANy Type added

---
+++ Task 8 (Any Type)/index.js
@@ -0,0 +1,7 @@
+var anyType;
+anyType = 12;
+console.log(anyType);
+anyType = 'hello';
+console.log(anyType);
+anyType = ['a', 'b', 2];
+console.log(anyType);
---
+++ Task 8 (Any Type)/index.ts
@@ -0,0 +1,10 @@
+let anyType: any;
+
+anyType = 12;
+console.log(anyType);
+
+anyType = 'hello';
+console.log(anyType);
+
+anyType = ['a', 'b', 2];
+console.log(anyType);
