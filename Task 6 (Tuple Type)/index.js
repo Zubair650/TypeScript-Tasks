86f3441e7a929d9214916564b1a9edfa206ff235
Zubair Ahmed <saad03261@gmail.com>
2023-08-17 19:11:07 +0600

    Tuples Type added

---
+++ Task 6 (Tuple Type)/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tupleArr;
+var tupleArr2;
+tupleArr = [12, 'zubair', true];
+tupleArr2 = [[52, 'saad']];
+console.log(tupleArr);
+console.log(tupleArr2);
---
+++ Task 6 (Tuple Type)/index.ts
@@ -0,0 +1,8 @@
+export { };
+let tupleArr: [number, string, boolean];
+let tupleArr2: [number, string][]
+tupleArr = [12, 'zubair', true];
+tupleArr2 = [[52, 'saad']];
+console.log(tupleArr);
+console.log(tupleArr2);
+
