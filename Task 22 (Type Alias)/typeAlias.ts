e3f48b208593284ed91cd9ab8e23248ea4cae2ee
Zubair Ahmed <saad03261@gmail.com>
2023-11-04 19:39:59 +0600

    Type Alias added

---
+++ Task 22 (Type Alias)/typeAlias.ts
@@ -0,0 +1,24 @@
+{
+    // type alias
+    type IDType = number;
+    type NameType = string;
+
+    type Student =
+        {
+            id: IDType,
+            name: NameType
+        }
+
+    let std1: Student =
+    {
+        id: 12,
+        name: 'Zubair'
+    }
+
+    let std2: Student =
+    {
+        id: 22,
+        name: 'Saad'
+    }
+
+}
